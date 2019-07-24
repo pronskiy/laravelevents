7eda1ac45613002a069d4ef966ccf7834c849a26
Roman Pronskiy <roman@pronskiy.com>
2019-07-24 10:52:20 +0300

    2019.2 sheclscript explainshell

--- newscript.sh
+++ newscript.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
-TMP_DIR="/tmp/$1"
+count_lines () {
+  local f=$1
+  l=$(wc -l "$f" | sed 's/^\([0-9]*\).*$/\1/')
+}
 
-cd $TMP_DIR;
+if [ $# -lt 1 ]
+then
+  echo "Usage: $0 file ..."
+  exit 1
+fi
+
+echo "$0 counts the lines of code"
+l=0
+n=0
+s=0
+while [ "$*" != ""  ]
+do
+        count_lines $1
+        echo "$1: $l"
+        n=$[ $n + 1 ]
+        s=$[ $s + $l ]
+	shift
+done
+
+echo "$n files in total, with $s lines in total"
