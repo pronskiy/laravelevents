96093afa99e2638ac14622a9668ae861fdae1b02
Roman Pronskiy <roman@pronskiy.com>
2019-07-24 10:41:43 +0300

    2019.2 shellscript path completion

--- newscript.sh
+++ newscript.sh
@@ -1,7 +1,42 @@
 #!/usr/bin/env bash
 
-cd ~/.composer/vendor/psr/log/Psr/Log/ || exit
+set -e
 
-for i in *.php ; do
-    echo "$i"
+if (( "$#" != 1 ))
+then
+    echo "Tag has to be provided"
+
+    exit 1
+fi
+
+CURRENT_BRANCH="5.8"
+VERSION=$1
+
+# Always prepend with "v"
+if [[ $VERSION != v*  ]]
+then
+    VERSION="v$VERSION"
+fi
+
+for REMOTE in auth broadcasting bus cache config console container contracts cookie database encryption events filesystem hashing http log mail notifications pagination pipeline queue redis routing session support translation validation view
+do
+    echo ""
+    echo ""
+    echo "Releasing $REMOTE";
+
+    TMP_DIR="/tmp/laravel-split"
+    REMOTE_URL="git@github.com:illuminate/$REMOTE.git"
+
+    rm -rf $TMP_DIR;
+    mkdir $TMP_DIR;
+
+    (
+        cd $TMP_DIR;
+
+        git clone $REMOTE_URL .
+        git checkout "$CURRENT_BRANCH";
+
+        git tag $VERSION
+        git push origin --tags
+    )
 done
