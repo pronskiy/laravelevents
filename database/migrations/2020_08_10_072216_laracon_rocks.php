4fbe682aeeba35f239068dc5a7405c2960b9e911
Roman Pronskiy <roman@pronskiy.com>
2020-08-10 16:39:28 +0300

    Add Laracon migration

---
+++ database/migrations/2020_08_10_072216_laracon_rocks.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class LaraconRocks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // 🤘
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
