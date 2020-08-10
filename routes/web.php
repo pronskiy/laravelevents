6810f8eeec1fe435a59765796a2ba2f25d5773ac
Roman Pronskiy <roman@pronskiy.com>
2020-08-10 16:53:19 +0300

    Add laracon route

--- routes/web.php
+++ routes/web.php
@@ -19,5 +19,5 @@ Route::get('/submit', 'SubmitController@index')->name('submit');
 Route::post('/submit', 'SubmitController@store');
 Route::get('/cp', function(){ return redirect('/cp/manage'); });
 Route::resource('/cp/manage', 'ManageEventsController');
-//Route::feeds();
 Auth::routes();
+Route::get('/laracon', fn() => 'Laracon 🔥');
